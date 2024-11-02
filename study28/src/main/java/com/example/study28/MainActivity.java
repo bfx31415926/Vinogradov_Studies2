af0d554db67f6cfd5dd2f6f6a3b9138e78879c84
bfx31415926 <smi-alexey@yandex.ru>
2024-11-03 00:23:39 +0300

    Добавил 28 урок

---
+++ study28/src/main/java/com/example/study28/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.study28;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    EditText etFName;
+    EditText etLName;
+
+    Button btnSubmit;
+
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        etFName = (EditText) findViewById(R.id.etFName);
+        etLName = (EditText) findViewById(R.id.etLName);
+
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, ViewActivity.class);
+        intent.putExtra("fname", etFName.getText().toString());
+        intent.putExtra("lname", etLName.getText().toString());
+        startActivity(intent);
+    }
+}
