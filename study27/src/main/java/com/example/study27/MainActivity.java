844c868e2b97ff46ee439da58297114dbf2a7d3c
bfx31415926 <smi-alexey@yandex.ru>
2024-11-03 00:15:33 +0300

    Добавил 27 урок

---
+++ study27/src/main/java/com/example/study27/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.study27;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Button btnTime = (Button) findViewById(R.id.btnTime);
+        Button btnDate = (Button) findViewById(R.id.btnDate);
+
+        btnTime.setOnClickListener(this);
+        btnDate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+
+        if(v.getId() == R.id.btnTime) {
+            intent = new Intent("ru.startandroid.intent.action.showtime");
+            startActivity(intent);
+        } else{ //v.getId() == R.id.R.id.btnDate
+                intent = new Intent("ru.startandroid.intent.action.showdate");
+                startActivity(intent);
+        }
+    }
+}
