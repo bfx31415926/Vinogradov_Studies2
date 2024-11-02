4fe576a4a50586aa10aa70a4814a8715bcc10126
bfx31415926 <smi-alexey@yandex.ru>
2024-11-02 17:43:32 +0300

    Добавил 21 урок

---
+++ study21/src/main/java/com/example/study21/ActivityTwo.java
@@ -0,0 +1,14 @@
+package com.example.study21;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+}
---
+++ study21/src/main/java/com/example/study21/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.study21;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    Button btnActTwo;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btnActTwo) {
+            Intent intent = new Intent(this, ActivityTwo.class);
+            startActivity(intent);
+        }
+    }
+}
